4375176a5d81dbc4f1172562b9fae45a8252e463
SaminaCodes135 <saminazahoor17@gmail.com>
2026-08-29 19:18:53 +0500

    Add subtraction program in C++

---
+++ 03-Arithmetic-Operators/subtraction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 20;
+    int y = 10;
+
+    int z = x - y;
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "z = x - y = " << z << endl;
+
+    return 0;
+}
