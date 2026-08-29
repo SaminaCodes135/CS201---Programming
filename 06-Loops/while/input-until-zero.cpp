26420389088e7032ae6a63e4c65c0d20f4e01369
SaminaCodes135 <saminazahoor17@gmail.com>
2026-08-29 21:08:08 +0500

    Add input loop until zero in input.cpp

---
+++ 06-Loops/while/input-until-zero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout << "Enter numbers (0 to stop): ";
+
+    cin >> number;
+
+    while (number != 0)
+    {
+        cout << "You entered: " << number << endl;
+
+        cin >> number;
+    }
+
+    cout << "Input stopped.";
+
+    return 0;
+}
