7fbb5c8475661b0a895756dec4d52858ab09becb
SaminaCodes135 <saminazahoor17@gmail.com>
2026-08-29 20:53:20 +0500

    Add countdown program using while loop

---
+++ 06-Loops/while/countdown.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 10;
+
+    // while loop repeats as long as the condition is true
+    while (number >= 1)
+    {
+        cout << number << endl;
+        number--;
+    }
+
+    return 0;
+}
